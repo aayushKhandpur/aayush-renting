435b55e7a7304a326f8773a958b616bf53f4fb11
aayushkhandpur <aayush.khandpur@gmail.com>
2016-01-05 13:51:41 +0530

    chng

--- app/code/core/Mage/Adminhtml/Block/Sales/Order/Grid.php
+++ app/code/core/Mage/Adminhtml/Block/Sales/Order/Grid.php
@@ -63,7 +63,7 @@ class Mage_Adminhtml_Block_Sales_Order_Grid extends Mage_Adminhtml_Block_Widget_
 
     protected function _prepareColumns()
     {
-
+      
         $this->addColumn('real_order_id', array(
             'header'=> Mage::helper('sales')->__('Order #'),
             'width' => '80px',
